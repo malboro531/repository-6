4e68d53b615325ea1721468043ae0f3b1efa5969
Denis <kovalev-denis_1996@mail.ru>
2022-10-25 09:25:03 +0300

    04

--- src/test/java/SimpleLocatorsTypesTest.java
+++ src/test/java/SimpleLocatorsTypesTest.java
@@ -56,6 +56,86 @@ public class SimpleLocatorsTypesTest {
         for(WebElement element : elements)
             logger.info("WebElement: " + element.getText());
     }
+    @Test
+    public void searchByTagTest(){
+        driver.get("https://kintern.myacademy.lanit.ru/login/briliant");
+        logger.info("Открыта страница myacademy.lanit - \" + \"https://kintern.myacademy.lanit.ru/login/briliant");
+        // Поиск элемента по тегу элемента
+        List<WebElement> elements = driver.findElements(By.tagName("form"));
+        for(WebElement element : elements)
+            logger.info("WebElement: " + element.getText());
+    }@Test
+    public void searchByLinkTextTest(){
+        driver.get("https://kintern.myacademy.lanit.ru/login/briliant");
+        logger.info("Открыта страница myacademy.lanit - \" + \"https://kintern.myacademy.lanit.ru/login/briliant");
+        // Поиск элемента по тексту ссылки
+        WebElement element = driver.findElement(By.linkText("забыли пароль?"));
+        logger.info("WebElement: " + element.getText());
+        element.click();
+        // Добавление задержки Thread.sleep, чтобы увидеть результат
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void searchByPartialLinkTextTest(){
+        driver.get("https://kintern.myacademy.lanit.ru/login/briliant");
+        logger.info("Открыта страница myacademy.lanit - \" + \"https://kintern.myacademy.lanit.ru/login/briliant");
+        // Поиск элемента по частичному тексту ссылки
+        WebElement element = driver.findElement(By.partialLinkText("забыли"));
+        logger.info("WebElement: " + element.getText());
+        element.click();
+        // Добавление задержки Thread.sleep, чтобы увидеть результат
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void searchByCssSelectorTest(){
+        driver.get("https://kintern.myacademy.lanit.ru/login/briliant");
+        logger.info("Открыта страница myacademy.lanit - \" + \"https://kintern.myacademy.lanit.ru/login/briliant");
+        // Поиск элемента по CSS селектору
+        WebElement element1 = driver.findElement(By.cssSelector("input#user_login_form_login"));
+        logger.info("WebElement: " + element1.getTagName());
+        logger.info("Id: " + element1.getAttribute("id"));
+        element1.sendKeys("Login");
+        // Поиск элемента по CSS селектору
+        WebElement element2 = driver.findElement(By.cssSelector("input#user_login_form_password"));
+        logger.info("WebElement: " + element2.getTagName());
+        logger.info("Id: " + element2.getAttribute("id"));
+        element2.sendKeys("Password");
+        // Добавление задержки Thread.sleep, чтобы увидеть результат
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void searchByXpathQueryTest(){
+        driver.get("https://kintern.myacademy.lanit.ru/login/briliant");
+        logger.info("Открыта страница myacademy.lanit - \" + \"https://kintern.myacademy.lanit.ru/login/briliant");
+        // Поиск элемента по XPATH запросу
+        WebElement element1 = driver.findElement(By.xpath(".//input[@id='user_login_form_login']"));
+        logger.info("WebElement: " + element1.getTagName());
+        logger.info("Id: " + element1.getAttribute("id"));
+        element1.sendKeys("Login");
+        // Поиск элемента по XPATH запросу
+        WebElement element2 = driver.findElement(By.xpath(".//input[@id='user_login_form_password']"));
+        logger.info("WebElement: " + element2.getTagName());
+        logger.info("Id: " + element2.getAttribute("id"));
+        element2.sendKeys("Password");
+        // Добавление задержки Thread.sleep, чтобы увидеть результат
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
     @AfterEach
     public void setDown() {
         if(driver != null) {
